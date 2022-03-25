575dc10e417a542971495dc4b04c33663eb19751
= <=>
2022-03-25 11:15:48 -0400

    Add custom tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      'sm': ['13px', '20px'],
+      'base': ['15px', '25px'],
+      'lg': ['16px', '24px'],
+      'xl': ['22px', '33px'],
+      '2xl': ['26px', '38px'],
+    },
+    extend: {
+      colors: {
+        'theme-offwhite': 'rgb(246, 248, 255)',
+        'theme-cool-100': 'rgb(105, 124, 154)',
+        'theme-cool-200': 'rgb(75, 106, 155)',
+        'theme-cool-300': 'rgb(43, 52, 66)',
+        'theme-warm-100': 'rgb(30, 42, 71)',
+        'theme-warm-200': 'rgb(20, 29, 47)',
+        'theme-primary': 'rgb(0, 121, 255)',
+      },
+      borderRadius: {
+        '10': '10px',
+      },
+      boxShadow: {
+        'card': '0px 16px 30px -10px rgba(70, 96, 187, 0.198567)',
+      },
+    },
   },
   plugins: [],
 }
